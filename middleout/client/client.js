aec8a8bb30e08532b52ddb97061facdb24302be6
Gabriel Pascualy <pascualy@umich.edu>
2016-02-21 05:47:24 -0500

    started interacting with web3

--- middleout/client/client.js
+++ middleout/client/client.js
@@ -1,4 +1,5 @@
 Template.client.contracts = function () {
+
   var i = 0; // This will be contract address
   var contracts =  [
     {id: "daa24d02bad7e9d6a80106db164bad9399a0423e", title: "Advertisement in GGBrown",
@@ -11,12 +12,10 @@ Template.client.contracts = function () {
 	description: "Blah Blah Blah",
 	price: "10 eth"}
   ];
-  console.log(contracts[0].id)
-  console.log(contracts[1].id)
   return contracts;
 };
 
-executeContract =  function(contractid, price){
+selectContract =  function(contractid, price){
 	var table = document.getElementById('contract_table');
 	var form = document.getElementById('contract_detail');
 	if(table.style.display !== 'none'){
@@ -29,5 +28,20 @@ executeContract =  function(contractid, price){
 	}
 	document.getElementById('contractValue').value = price;
 	document.getElementById('contractAddress').value = contractid;
-
 };
+
+executeContract = function(contractid, price){
+
+}
+
+transaction = function() {
+	var transactionObject = {to:"0x9f38a38c3492ed0737c27ab7c3b52c4b2d933392", from: web3.eth.coinbase, value: web3.toWei(.000000000001, "ether")};
+	try {
+		web3.personal.unlockAccount(web3.eth.coinbase, "middleout");
+	}
+	catch(err){
+		console.log("nope")
+	}
+	web3.eth.sendTransaction(transactionObject);
+}
+
--- middleout/client/home.js
+++ middleout/client/home.js
@@ -1,9 +1,12 @@
 
-Meteor.startup(function () {
 
-});
 
 Router.route('/', function(){
+	if(typeof web3 === 'undefined'){
+		web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+		console.log("aasasdfasdfdfasd");
+		console.log(web3.eth.coinbase);
+	}
   this.render('home');
 });
 
